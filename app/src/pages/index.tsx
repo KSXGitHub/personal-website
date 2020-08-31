c024cfe41171d0d90ce494c868f2a04e8151385e
khai96_ <hvksmr1996@gmail.com>
2020-08-31 10:58:30 +0700

    Add something

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+const title = "Khải's Profile"
+
 const Home = () =>
   <>
     <Helmet>
       <meta charSet='UTF-8' />
-      <title>Profile</title>
+      <title>{title}</title>
     </Helmet>
+    <main>
+      <h1>{title}</h1>
+    </main>
   </>
 
 export default Home
