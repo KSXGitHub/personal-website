56c6beb756d810e3c0a2032d22dc2c249b5375db
khai96_ <hvksmr1996@gmail.com>
2020-09-18 18:06:59 +0700

    Properly align

--- packages/components/Main.tsx
+++ packages/components/Main.tsx
@@ -1,24 +1,43 @@
 import React from 'react'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 import GitHubIcon from '@material-ui/icons/GitHub'
 
 export interface MainAttr {
   readonly title: string
 }
 
-export const Main = (attr: MainAttr) =>
-  <main>
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+      marginRight: theme.spacing(2),
+    },
+  })
+)
+
+export function Main(attr: MainAttr) {
+  const classes = useStyles()
+
+  return <main className={classes.root}>
     <AppBar position='static'>
       <Toolbar>
-        <Typography variant='h3'>{attr.title}</Typography>
-        <GitHubIcon />
+        <Typography className={classes.title} variant='h3'>{attr.title}</Typography>
+        <Button>
+          <GitHubIcon />
+        </Button>
       </Toolbar>
     </AppBar>
     <Container>
     </Container>
   </main>
+}
 
 export default Main
