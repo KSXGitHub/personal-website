4299c745585cf49d22713c88e14b6dba339d6280
khai96_ <hvksmr1996@gmail.com>
2020-09-19 11:57:31 +0700

    Make a flex box

--- packages/components/Profile.tsx
+++ packages/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 import Avatar from '@material-ui/core/Avatar'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -13,6 +14,9 @@ export interface ProfileAttr {
 
 const useStyles = makeStyles(theme =>
   createStyles({
+    personalInformation: {
+      display: 'flex',
+    },
     avatar: {
       marginTop: theme.spacing(2),
       width: theme.spacing(25),
@@ -25,15 +29,17 @@ export function Profile(attr: ProfileAttr) {
   const classes = useStyles()
 
   return <>
-    <Avatar
-      className={classes.avatar}
-      alt={attr.avatarAlt}
-      src={attr.avatarSrc}
-    />
-    <Card>
-      <CardContent>
-        <Typography variant='h4'>{attr.fullName}</Typography>
-      </CardContent>
-    </Card>
+    <Container className={classes.personalInformation}>
+      <Avatar
+        className={classes.avatar}
+        alt={attr.avatarAlt}
+        src={attr.avatarSrc}
+      />
+      <Card>
+        <CardContent>
+          <Typography variant='h4'>{attr.fullName}</Typography>
+        </CardContent>
+      </Card>
+    </Container>
   </>
 }
