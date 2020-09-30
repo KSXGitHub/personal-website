477f7a82e3b9c4f00c91e1ea22774355e2f11be3
khai96_ <hvksmr1996@gmail.com>
2020-09-30 17:10:16 +0700

    Clean @tools/publish-web-pages

--- tools/publish-web-pages/lib/config.ts
+++
@@ -1,6 +0,0 @@
-import { Options } from '@tools/gh-pages'
-
-/**
- * List of additional options to iterate over
- */
-export const optionsList: Options[] = [{}]
--- tools/publish-web-pages/lib/index.ts
+++ tools/publish-web-pages/lib/index.ts
@@ -1,19 +1,9 @@
 import process from 'process'
-import { publish, Options } from '@tools/gh-pages'
 import places from '@tools/places'
-import { optionsList } from './config'
-
-const DEFAULT_OPTIONS: Options = { dotfiles: true }
 
 export async function main() {
   process.chdir(places.project)
   console.info('Publishing website...')
-
-  for (const options of optionsList) {
-    const fullOptions = { ...DEFAULT_OPTIONS, ...options }
-    console.info('web-pages>', fullOptions)
-    await publish(places.dist, fullOptions)
-  }
 }
 
 export default main
