72436d4d6ea0f66112a7e75f1dc7d456de802127
khai96_ <hvksmr1996@gmail.com>
2020-10-01 14:18:35 +0700

    Use ReactGA

--- packages/components/GoogleAnalytics.tsx
+++ packages/components/GoogleAnalytics.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { initialize, pageview } from 'react-ga'
 
 export interface GoogleAnalyticsAttr {
   readonly trackerId: string
@@ -6,34 +7,9 @@ export interface GoogleAnalyticsAttr {
 
 export function GoogleAnalytics(attr: GoogleAnalyticsAttr) {
   React.useEffect(() => {
-    if (typeof document === 'undefined') return
-
-    type ScriptInitializer = (script: HTMLScriptElement) => void
-
-    const scriptInitializers: readonly ScriptInitializer[] = [
-      script => {
-        script.async = true
-        script.src = `https://www.googletagmanager.com/gtag/js?id=${attr.trackerId}`
-      },
-
-      script => {
-        script.text = `
-          window.dataLayer = window.dataLayer || [];
-          function gTag(){dataLayer.push(arguments);}
-          gTag('js', new Date());
-          gTag('config', ${JSON.stringify(attr.trackerId)});
-        `
-      },
-    ]
-
-    for (const initScript of scriptInitializers) {
-      const script = document.createElement('script')
-      initScript(script)
-      script.addEventListener('load', () => {
-        document.body.removeChild(script)
-      })
-      document.body.appendChild(script)
-    }
+    if (typeof window === 'undefined') return
+    initialize(attr.trackerId)
+    pageview(window.location.href)
   })
 
   return <></>
