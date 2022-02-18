76199ac278f2e6bc56cc8656c63391e9e692aabc
khai96_ <hvksmr1996@gmail.com>
2022-02-18 10:08:23 +0700

    Remove google plugins

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -7,19 +7,4 @@ exports.plugins = [
 
   'gatsby-plugin-react-helmet',
   'gatsby-plugin-material-ui',
-
-  {
-    resolve: 'gatsby-plugin-google-tagmanager',
-    options: {
-      id: 'UA-179378718-1',
-    },
-  },
-
-  {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      trackingId: 'UA-179378718-1',
-      respectDNT: true,
-    },
-  },
 ]
